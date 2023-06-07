9f2ac07ea47f713de182e23209383bc3a6247cf4
EnockCity <enockmwanzia17@gmail.com>
2023-06-07 14:04:24 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,12 +15,31 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 
-
 		return (-1);
-
 
 	return (find_sqrt(0, n));
 
+}
+
+/**
+ * find_sqrt - checks for square root based on number
+ * @n: the number we are looking for the square root of
+ * @sqrt: the possible root
+ * Return: return the root or -1
+ */
+
+int find_sqrt(int sqrt, int n)
+{
+
+	if (sqrt * sqrt > n)
+
+		return (-1);
+
+	if (sqrt * sqrt == n)
+
+		return (sqrt);
+
+	return (find_sqrt(sqrt + 1, n));
 
 }
 
