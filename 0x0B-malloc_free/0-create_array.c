0ed3a5cc977aa323d157830914921c517341b725
EnockCity <enockmwanzia17@gmail.com>
2023-06-13 11:07:08 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *create_array - function that creates a char array
+ *@size: size of the array
+ *@c: character to be looked for
+ * Return: NULL if size = 0
+ * or return pointer to array or NULL if fails
+ */
+char *create_array(unsigned int size, char c)
+{
+
+	if (size == 0)
+
+	{
+
+		return (NULL);
+	}
+
+	char *array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+
+	{
+
+		return (NULL);
+
+	}
+
+	for (unsigned int k = 0; k < size; ++k)
+
+	{
+
+		array[k] = c;
+
+	}
+
+	return (array);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,9 @@
+0x0B. C - malloc, free
+This program aims to focus dynamic memory allocation.
+The functions involved in memory allocation are:
+1.malloc ();
+2.calloc ();
+3.realloc ();
+4.free ();
+In our case we will focus entirely on malloc function which allocates a specific number of bytes in memory and returns a pointer to the allocated memory as well as free function which frees the memory that was initially allocated.
+
