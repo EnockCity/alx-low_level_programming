6042641222e7119e4b91890c7bec24b2b1dfd5ff
EnockCity <enockmwanzia17@gmail.com>
2023-06-13 11:09:32 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -28,11 +28,11 @@ char *create_array(unsigned int size, char c)
 
 	}
 
-	for (unsigned int k = 0; k < size; ++k)
+	for (unsigned int i = 0; i < size; ++i)
 
 	{
 
-		array[k] = c;
+		array[i] = c;
 
 	}
 
