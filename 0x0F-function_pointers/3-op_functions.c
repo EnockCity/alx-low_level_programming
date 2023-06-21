beb8c8c6f30c3167cf76e7a1d8d1497b37338174
EnockCity <enockmwanzia17@gmail.com>
2023-06-21 07:00:56 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Operation of addition.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Addition of ops.
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Operation of substraction.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Substraction of ops.
+ */
