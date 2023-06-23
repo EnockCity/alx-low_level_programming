faaec4e6f11b5370813bff9dcaf7334eab76d867
EnockCity <enockmwanzia17@gmail.com>
2023-06-22 17:35:35 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,13 +7,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int j, sum = 0;
+	unsigned int i, sum = 0;
 	va_list list;
 
 	if (n == 0)
 		return (0);
 	va_start(list, n);
-	for (j = 0; j < n; ++j)
+	for (i = 0; i < n; ++j)
 		sum += va_arg(list, int);
 	va_end(list);
 	return (sum);
