5dbd0932b54555ce7c2f6318cff855cf32106731
EnockCity <enockmwanzia17@gmail.com>
2023-08-21 21:15:43 +0300

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -p .. https://raw.githubusercontent.com/
+monoprosito/holbertonschool-low_level_programming/
+master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
